92def5e4520ca88d92d3c7957a65879b92d59da1
aneritagirl <inboxanita@outlook.com>
2025-08-20 00:07:03 +0100

    Update huawei.js

--- styles/scripts/huawei.js
+++ styles/scripts/huawei.js
@@ -1,1 +1,6 @@
 
+// simple scroll-reveal
+const io = new IntersectionObserver((ents)=>{
+  ents.forEach(e=>{ if(e.isIntersecting) e.target.classList.add('is-in'); });
+},{threshold:.15});
+document.querySelectorAll('.reveal').forEach(n=>io.observe(n));
